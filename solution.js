2411c14fadcd59cbed9d16bbef6ef1de3a5ea1be
meagan17 <mramo4555@withs.org>
2016-04-15 14:30:22 +0000

    color-changer

--- solution.js
+++ solution.js
@@ -1,1 +1,16 @@
+var colors =["red", "blue", "orange", "green"];
+function changeColor(){
+ var randomIndex= Math.floor(Math.random() * colors.length);
+    $("body").css("background", colors[randomIndex]);
+}
 
+function addColor(){
+  var newColor= $("#newColor").val();
+    colors[colors.length]= newColor;
+        $("body").css("background",  colors[colors.length]);
+}
+
+$(document).ready(function(){
+   $("#change").click(changeColor);
+    $("#add").click(addColor);
+});
